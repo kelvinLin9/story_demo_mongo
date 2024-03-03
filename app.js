5d3b88040869738793db6e3b12cb7877a0386d06
kelvin <kelvin80121@hotmail.com>
2024-03-03 20:23:06 +0800

    Add new routes for brick and story models

--- app.js
+++ app.js
@@ -4,20 +4,23 @@
 // 4. views 視圖
 // MVC
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var postsRouter = require('./routes/posts');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const brickRouter = require('./routes/brick');
+const storyRouter = require('./routes/story');
+
 const mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb+srv://kelvin80121:iEK0Zh2uWzgUnNLK@cluster0.x7cwh1p.mongodb.net/')
+mongoose.connect('mongodb+srv://kelvin80121:iSKM5Vf2UE7KIZPL@jstorydemo.fsnxxtf.mongodb.net/?retryWrites=true&w=majority&appName=jStoryDemo')
     .then(res=> console.log("連線資料成功"));
 
 
@@ -32,5 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+app.use('/brick', brickRouter);
+app.use('/story', storyRouter);
 
 module.exports = app;
--- models/brickModel.js
+++ models/brickModel.js
@@ -1,29 +1,70 @@
-const mongoose = require('mongoose')
-const brickSchema = new mongoose.Schema(
-    {
-      content: {
-        type: String,
-        required: [true, 'Content 未填寫']
-      },
-      image: {
-        type:String,
-        default:""
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false
-      },
-      name: {
-          type: String,
-          required: [true, '貼文姓名未填寫']
-      },
-      likes: {
-          type:Number,
-          default:0
-        }
-    }
-);
-const Post = mongoose.model('Post', postSchema);
+const mongoose = require('mongoose');
 
-module.exports = Post;
+const mediaSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true, match: [/^https?:\/\/.+/i, 'Invalid URL format'] } 
+});
+
+const imageSizesSchema = new mongoose.Schema({
+  "9_16": [{ type: String, match: [/^https?:\/\/.+/i, 'Invalid URL format'] }],
+  "16_9": [{ type: String, match: [/^https?:\/\/.+/i, 'Invalid URL format'] }],
+  "1_1": [{ type: String, match: [/^https?:\/\/.+/i, 'Invalid URL format'] }]
+});
+
+const contentSchema = new mongoose.Schema({
+  media: { type: mediaSchema, required: true },
+  type: { type: String, required: true, enum: ['Stream', 'Story', 'Photos', ''] },
+  channel: { type: String, required: true },
+  image: { type: imageSizesSchema, required: true }
+});
+
+const brickSchema = new mongoose.Schema({
+  kind: { type: String, required: [true, 'Kind 未填寫'] },
+  tags: { type: [String], required: [true, 'Tags 未填寫'] },
+  content: { type: contentSchema, required: true }
+}, 
+{ 
+  timestamps: true,
+  versionKey: false
+}); 
+
+
+const Brick = mongoose.model('Brick', brickSchema);
+
+module.exports = Brick;
+
+
+
+// const mongoose = require('mongoose')
+// const brickSchema = new mongoose.Schema(
+//     {
+//       content: {
+//         type: String,
+//         required: [true, 'Content 未填寫']
+//       },
+//       created_at: {
+//         type: Date,
+//         default: Date.now(),
+//         select: false
+//       },
+//       kind: {
+//         type: String,
+//         required: [true, 'kind未填寫']
+//       },
+//       tags: {
+//         type: String,
+//         required: [true, 'tags未填寫']
+//       },
+//       likes: {
+//         type:Number,
+//         default:0
+//       },
+//       image: {
+//         type:String,
+//         default:""
+//       },
+//     }
+// );
+// const Post = mongoose.model('Post', postSchema);
+
+// module.exports = Post;
